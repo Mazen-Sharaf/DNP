4648c0ae2e88f610e669130a10bb24b04f5f2e5e
Mazen Sharaf <mazen.sharaf03@gmail.com>
2025-09-29 23:52:54 +0200

    Assignment 3

--- CLI/Program.cs
+++ CLI/Program.cs
@@ -1,11 +1,12 @@
 ﻿using CLI.UI;
-using InMemoryRepositories;
+using fileRepositories;
+using FileRepositories;
 using RepositoryContracts;
 
 Console.WriteLine("Starting CLI app!");
-IPostRepository posts = new PostInMemoryRepository();
-IUserRepository users = new UserInMemoryRepository();
-ISubforumRepository subforums = new SubforumMemoryRepository();
+IPostRepository posts = new PostFileRepository();
+IUserRepository users = new UserFileRepository();
+ISubforumRepository subforums = new SubforumFileRepository();
 
 CLIApp app = new CLIApp(posts, users, subforums);
 await app.StartAsync();
